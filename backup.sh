7d7587da43f526c3dc87622f173ae3fbd7947dec
sh1nez <biv888iii888@yandex.ru>
2023-10-30 14:16:50 +0300

    2023-10-30 14:16

---
+++ backup.sh
@@ -0,0 +1,43 @@
+#!/bin/env sh
+target_dir="${HOME}/dotfiles"
+dot_directories="
+        /etc/portage/
+	/etc/systemd/
+
+        ${HOME}/.config/alacritty/
+	${HOME}/.config/cava/
+	${HOME}/.config/eww/
+	${HOME}/.config/fontconfig/
+	${HOME}/.config/foot/
+	${HOME}/.config/htop/	
+	${HOME}/.config/kitty/
+	${HOME}/.config/mako/
+	${HOME}/.config/neofetch/
+	${HOME}/.config/nnn/
+	${HOME}/.config/pavucontrol.ini
+	${HOME}/.config/sway/
+	${HOME}/.config/systemd/
+	${HOME}/.config/Tunar/
+	${HOME}/.config/xdg-desktop-portal/
+
+        ${HOME}/.themes/
+        ${HOME}/.icons/
+	${HOME}/.mozilla/firefox/owin18hn.default-release/chrome/
+        
+"
+
+
+for dir in $dot_directories; do
+        if [ ! -d ${target_dir}${dir} ]; then
+                mkdir --parents ${target_dir}${dir}
+        fi
+done
+
+
+for dir in $dot_directories; do
+        rsync -auv --delete ${dir} ${target_dir}${dir}
+done
+equery list "*" > ${target_dir}/all_pkg
+
+date=$(date +"%Y-%m-%d %H:%M")
+git -C "$target_dir" add -A && git -C "$target_dir" commit -m "$date" && git -C "$target_dir" push
