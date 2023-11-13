36c32fff50b56ad14bfc63cb8a6a88e86b445ecf
sh1nez <biv888iii888@yandex.ru>
2023-11-13 03:48:40 +0300

    2023-11-13 03:48

--- home/sad1/.config/nvim/lua/plugins/floaterm.lua
+++ home/sad1/.config/nvim/lua/plugins/floaterm.lua
@@ -1,28 +1,48 @@
 local function execute_command(com)
     vim.cmd(":w")
-    vim.cmd(":FloatermNew!" .. com)
+    vim.cmd(":FloatermNew! " .. com)
 end
+
+local switch_cases = {
+    cpp = function(file)
+    -- vilocal file_directory = vim.fn.expand("%:h")   
+	    local file_directory = vim.fn.fnamemodify(file, ":h")
+        local file_name = vim.fn.expand("%:t:r")
+        execute_command("cd " .. file_directory .. " && clang++ " .. file .. ' -o ' .. file_name .. ' && "' .. file_directory .. '/' .. file_name .. '"')
+	--	execute_command("cd " .. vim.fn.expand("%:h") .. " clang++ -o " .. vim.fn.expand("%:h"):match("(.*)%..*") .. " " .. file .. " && . " .. file:match("(.*)%..*"))
+
+    --    execute_command("cd " .. file_directory .. " && clang++ -o " .. file:match("(.*)%..*") .. " " .. file)
+    end,
+
+    c = function(file)
+		local file_directory = vim.fn.fnamemodify(file, ":h")
+        local file_name = vim.fn.expand("%:t:r")
+        execute_command("cd " .. file_directory .. " && clang" .. file .. ' -o ' .. file_name .. ' && "' .. file_directory .. '/' .. file_name .. '"')
+
+    end,
+    py = function(file)
+        execute_command("python3 " .. file)
+    end,
+    lua = function(file)
+        execute_command("lua " .. file)
+    end,
+    sh = function(file)
+        execute_command("bash " .. file)
+    end,
+    default = function(file)
+        vim.cmd(":w")
+        vim.cmd('echo "Unsupported file type: ' .. file_extension .. '"')
+    end,
+}
 
 function run_or_compile(file)
     local file_extension = file:match(".*%.(.*)")
-	if not file_extension then
-		vim.cmd(":w")
-		return
-	end
-    if file_extension == "cpp" or file_extension == "c" then
-        execute_command("gcc -o " .. file:match("(.*)%..*") .. " " .. file .. " && ./" .. file:match("(.*)%..*"))
-    elseif file_extension == "py" then
-        execute_command("python3 " .. file)
-    elseif file_extension == "lua" then
-        execute_command("lua " .. file)
-    elseif file_extension == "sh" then
-        execute_command("bash " .. file)
-    else
+    if not file_extension then
         vim.cmd(":w")
-        vim.cmd('echo "Unsupported file type: ' .. file_extension .. '"')
+        return
     end
+    (switch_cases[file_extension] or switch_cases.default)(file)
 end
-
--- Задаем бинд для выполнения функции при нажатии <leader>r
-vim.api.nvim_set_keymap('n', '<leader>r', [[:lua run_or_compile(vim.fn.expand("%:p"))<CR>]], { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>rt', ':FloatermNew<CR>')
+vim.api.nvim_set_keymap('n', '<leader>rr', [[:lua run_or_compile(vim.fn.expand("%:p"))<CR>]], { noremap = true, silent = true })
 
