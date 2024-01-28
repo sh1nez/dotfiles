ebb8888a9e2a94f8b628586f3dde416266f008d5
sh1nez <biv888iii888@yandex.ru>
2024-01-28 22:36:01 +0300

    2024-01-28 22:36

--- home/sad1/.config/nvim/lua/core/plugins.lua
+++ home/sad1/.config/nvim/lua/core/plugins.lua
@@ -1,55 +1,52 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
 vim.opt.rtp:prepend(lazypath)
+-- if not vim.loop.fs_stat(lazypath) then
+-- 	vim.fn.system({
+--  		"git",
+--  		"clone",
+--  		"--filter=blob:none",
+--  		"https://github.com/folke/lazy.nvim.git",
+--  		"--branch=stable", -- latest stable release
+-- 		lazypath,
+-- 	})
+-- end
+
 require("lazy").setup({
-{"nvim-neo-tree/neo-tree.nvim",
-    	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    	},
-},
-{'nvim-treesitter/nvim-treesitter'},
-{"ellisonleao/gruvbox.nvim"},
-{'neovim/nvim-lspconfig'},
-{'joshdick/onedark.vim'},
-{'olimorris/onedarkpro.nvim'},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+	},
+	{ 'nvim-treesitter/nvim-treesitter' },
+	{ "ellisonleao/gruvbox.nvim" },
+	{ 'neovim/nvim-lspconfig' },
+	{ 'joshdick/onedark.vim' },
+	{ 'olimorris/onedarkpro.nvim' },
 
-{'neovim/nvim-lspconfig'},
-{'williamboman/mason-lspconfig.nvim'},
-{"williamboman/mason.nvim"},
+	{ 'neovim/nvim-lspconfig' },
+	{ 'williamboman/mason-lspconfig.nvim' },
+	{ "williamboman/mason.nvim",             lazy = true },
 
-{'hrsh7th/cmp-nvim-lsp'},
-{'hrsh7th/cmp-buffer'},
-{'hrsh7th/cmp-path'},
-{'hrsh7th/cmp-cmdline'},
-{'hrsh7th/nvim-cmp'}, --dependencies = { 'quangnguyen30192/cmp-nvim-ultisnips' } },
+	{ 'hrsh7th/cmp-nvim-lsp' },
+	{ 'hrsh7th/cmp-buffer' },
+	{ 'hrsh7th/cmp-path' },
+	{ 'hrsh7th/cmp-cmdline' },
+	{ 'hrsh7th/nvim-cmp' }, --dependencies = { 'quangnguyen30192/cmp-nvim-ultisnips' } },
 
-{'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' }},
+	{ 'nvim-telescope/telescope.nvim',       branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
 
-{'terrortylor/nvim-comment'},
-{'voldikss/vim-floaterm'},
-{'mfussenegger/nvim-dap'},
-{'mfussenegger/nvim-dap-python'},
-{'eandrju/cellular-automaton.nvim'},
-{'jiangmiao/auto-pairs'},
+	{ 'terrortylor/nvim-comment' },
+	{ 'voldikss/vim-floaterm' },
+	{ 'eandrju/cellular-automaton.nvim' },
+	{ 'jiangmiao/auto-pairs',                lazy = true },
+	{ 'lukas-reineke/indent-blankline.nvim', main = "ibl",     opts = { scope = { enabled = false } } },
 
-{'lukas-reineke/indent-blankline.nvim', main="ibl", opts = {scope = { enabled = false}}},
-
-{"L3MON4D3/LuaSnip", version = "v2.*",build = "make install_jsregexp",},
-{'saadparwaiz1/cmp_luasnip'},
-{ "kylechui/nvim-surround", version = "main", event = "VeryLazy" },
-
+	{ "L3MON4D3/LuaSnip",                    version = "v2.*", build = "make install_jsregexp" },
+	{ 'saadparwaiz1/cmp_luasnip' },
+	{ "kylechui/nvim-surround",              version = "*",    event = "VeryLazy", }
 })
-
