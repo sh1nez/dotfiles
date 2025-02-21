1b7d04bcd9eaf589137a1e78d965d80e0a320feb
2happy <biv888iii888@yandex.ru>
2025-02-21 23:59:19 +0300

    2025-02-21 23:59

--- home/2happy/.config/nvim/lua/plugins/telescope.lua
+++ home/2happy/.config/nvim/lua/plugins/telescope.lua
@@ -12,11 +12,12 @@ vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
 
 require('telescope').setup {
 	defaults = {
-		layout_strategy = "cursor",
+		-- layout_strategy = "cursor",
 		layout_config = {
 			width = 0.999,
 			height = 0.999,
-			preview_width = 0.5
+			preview_width = 0.5,
+			prompt_position = 'bottom',
 		},
 	},
 	pickers = {
@@ -25,3 +26,9 @@ require('telescope').setup {
 		},
 	},
 }
+
+vim.api.nvim_set_keymap('n', '<leader>lr', '<cmd>lua require("telescope.builtin").lsp_references()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ls', '<cmd>lua require("telescope.builtin").lsp_document_symbols()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>lw', '<cmd>lua require("telescope.builtin").lsp_workspace_symbols()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>li', '<cmd>lua require("telescope.builtin").lsp_implementations()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>lt', '<cmd>lua require("telescope.builtin").lsp_type_definitions()<CR>', { noremap = true, silent = true })
