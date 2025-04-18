f28dce35cd36fdd2adc157022f9d291ca67f8d61
2happy <biv888iii888@yandex.ru>
2025-04-18 22:17:49 +0300

    update backup script

--- backup.sh
+++ backup.sh
@@ -1,39 +1,41 @@
 #!/bin/env sh
 
-
 target_dir="$PWD"
+
+if [ $# -gt 0 ]; then
+    commit_msg="$1"
+else
+    commit_msg=$(date +"%Y-%m-%d %H:%M")
+fi
+
 dot_directories="
     ${HOME}/.config/alacritty/
-	${HOME}/.config/kitty/
-
-	${HOME}/.config/cava/
-	${HOME}/.config/mako/
-	${HOME}/.config/neofetch/
-
-	${HOME}/.config/bspwm/ 
-	${HOME}/.config/sxhkd/
-	${HOME}/.config/fontconfig/
+    ${HOME}/.config/kitty/
+    ${HOME}/.config/cava/
+    ${HOME}/.config/mako/
+    ${HOME}/.config/neofetch/
+    ${HOME}/.config/bspwm/
+    ${HOME}/.config/sxhkd/
+    ${HOME}/.config/fontconfig/
     ${HOME}/.config/gtk-3.0/
-	${HOME}/.config/pavucontrol.ini
-	${HOME}/.xinitrc
-
-	${HOME}/.config/btop/	
+    ${HOME}/.config/pavucontrol.ini
+    ${HOME}/.xinitrc
+    ${HOME}/.config/btop/
     ${HOME}/.config/nvim/
-	${HOME}/.config/fish/
-	${HOME}/.config/tmux/
+    ${HOME}/.config/fish/
+    ${HOME}/.config/tmux/
 "
-# bspwm dots https://github.com/gh0stzk/dotfiles
 
 for dir in $dot_directories; do
-        if [ ! -d "${target_dir}""${dir}" ]; then
-                mkdir --parents "${target_dir}""${dir}"
-        fi
+    if [ ! -d "${target_dir}${dir}" ]; then
+        mkdir --parents "${target_dir}${dir}"
+    fi
 done
-
 
 for dir in $dot_directories; do
-        rsync -auv --delete "${dir}" "${target_dir}""${dir}"
+    rsync -auv --delete "${dir}" "${target_dir}${dir}"
 done
 
-date=$(date +"%Y-%m-%d %H:%M")
-git -C "$target_dir" add -A && git -C "$target_dir" commit -m "$date" && git -C "$target_dir" push
+git -C "$target_dir" add -A && \
+git -C "$target_dir" commit -m "$commit_msg" && \
+git -C "$target_dir" push
